71c497d43a34a1eaadc811b395ccdc4c93f8b111
Aliaksandr Zarudni <azarudniy@yandex.ru>
2021-05-24 23:48:21 +0300

    feat: part of second

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.h
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.h
@@ -3,5 +3,6 @@
 @interface PlayersSeparator : NSObject
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray;
+- (int) factorial:(int)n;
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,60 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSMutableArray<NSNumber *> *upperArray = [NSMutableArray new];
+    NSMutableArray<NSNumber *> *lowerArray = [NSMutableArray new];
+        
+    for (NSNumber *item in ratingArray) {
+        if ([upperArray count] == 0 || [item intValue] > [[upperArray lastObject] intValue]) {
+            [upperArray addObject:item];
+        } else {
+            if ([upperArray count] == 1) {
+                upperArray[0] = item;
+            } else {
+                NSNumber *tempNumber = [upperArray lastObject];
+                [upperArray removeLastObject];
+                if ([item intValue] > [[upperArray lastObject] intValue]) {
+                    [upperArray addObject:item];
+                } else {
+                    [upperArray addObject:tempNumber];
+                }
+            }
+        }
+    }
+    
+    for (NSNumber *item in ratingArray) {
+        if ([lowerArray count] == 0 || [item intValue] < [[lowerArray lastObject] intValue]) {
+            [lowerArray addObject:item];
+        } else {
+            if ([lowerArray count] == 1) {
+                lowerArray[0] = item;
+            } else {
+                NSNumber *tempNumber = [lowerArray lastObject];
+                [lowerArray removeLastObject];
+                if ([item intValue] < [[lowerArray lastObject] intValue]) {
+                    [lowerArray addObject:item];
+                } else {
+                    [lowerArray addObject:tempNumber];
+                }
+            }
+        }
+    }
+    int numberCombs = 0;
+    if ([upperArray count] > 2) {
+        numberCombs += [self factorial:[upperArray count]]/([self factorial:3]*[self factorial:[upperArray count]-3]);
+    }
+    if ([lowerArray count] > 2) {
+        numberCombs += [self factorial:[lowerArray count]]/([self factorial:3]*[self factorial:[lowerArray count]-3]);
+    }
+return numberCombs;
+}
+
+- (int)factorial:(int)n
+{
+    if (n == 0)
+        return 1;
+    return n * [self factorial:n-1];
 }
 
 @end
+
